66ad4b8b1c6cd210cd30e704fe383f6c21652f0f
Lukyanchuk Nikolay <lukyanchuknikolay@gmail.com>
2018-01-12 14:34:36 +0300

    remove developer stuff

--- test/invalidEnum.js
+++ test/invalidEnum.js
@@ -4,10 +4,6 @@ const { parseCommand } = require('../testIndex');
 import assert from 'assert';
 
 describe('Enum option with invalid value', function() {
-  after(function() {
-    // runs after all tests in this block
-  });
-  
   it('the enum\'d option should have it\'s default value', function() {
     const args = [
       '/usr/local/bin/node',
